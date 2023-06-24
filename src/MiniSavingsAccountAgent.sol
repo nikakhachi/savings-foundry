f9bfa9fb7f3bb41ac44a27a1e66202a6b9fc5943
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-06-24 09:45:40 +0400

    add MiniSavingsAccountAgent.sol

---
+++ src/MiniSavingsAccountAgent.sol
@@ -0,0 +1,194 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "openzeppelin/token/ERC20/IERC20.sol";
+
+/// @dev Custom errors
+error InvalidToken();
+error InsufficientBalance();
+error NonAgent();
+error ProposalNotFound();
+error AlreadyVoted();
+error InvalidDelegate();
+error VotingEnded();
+error VotingInProgress();
+error ProposalNotPending();
+
+/// @title MiniSavingsAccountAgent
+/// @author Nika Khachiashvili
+/// @dev The contract contains variables and function for proposing and voting for adding new tokens
+/// @dev Lots of other functions can also be added for voting for example updating the annual interest rate,
+/// @dev adding or revoking the current agents, or even proposing a different tiers with different interest rates for tokens
+contract MiniSavingsAccountAgent {
+    event NewTokenProposed(uint proposalId);
+    event NewTokenProposalVoted(
+        address indexed voter,
+        address delegate,
+        uint proposalId,
+        bool isInFavor
+    );
+    event NewTokenProposalFailed(uint proposalId);
+    event NewTokenProposalExecuted(uint proposalId);
+
+    /// @dev The agent is an address which is able to vote for proposals
+    mapping(address => bool) public isAgent;
+    uint agentsCount;
+
+    /// @dev Vote Status of the agent in reference to the specific proposal
+    enum Vote {
+        NO_VOTE,
+        IN_FAVOR,
+        AGAINST
+    }
+
+    /// @dev Status of the proposal
+    enum ProposalStatus {
+        PENDING,
+        EXECUTED,
+        FAILED
+    }
+
+    /// @dev When proposing or executing the addition of a new token, it will be essentioal for contract
+    /// @dev to have this minimum balance of the token to ensure safe giving of the interest
+    uint public constant requiredTokenBalanceUponAdding = 10000 * 10 ** 18;
+
+    /// @dev Interval between proposing new token addition and executing it
+    /// @dev During this interval agents will be able to vote
+    uint public newTokenProposalDuration = 1 weeks;
+
+    /// @dev mapping of tokens with their annual rates
+    /// @dev If the annual rate for token is 0 (default), it means that the token isn't supported
+    mapping(address => uint16) public tokenAnnualRates;
+
+    struct NewTokenProposal {
+        uint id;
+        address token;
+        uint voteEndsAt;
+        uint votesNeededToPass;
+        uint against;
+        uint inFavor;
+        ProposalStatus status;
+        uint16 annualRate;
+    }
+
+    mapping(uint => NewTokenProposal) public newTokenProposals;
+    uint newTokenProposalsCount;
+
+    /// @dev mapping keeping track of agent votes for the specific proposals
+    mapping(address => mapping(uint => Vote)) public agentVotes;
+
+    /// @dev mapping keeping track of address delegates
+    mapping(address => address) public delegates;
+
+    /// @dev Modifierthat ensures only agent who is able to vote can call the function
+    modifier onlyAgent() {
+        if (!isAgent[msg.sender]) revert NonAgent();
+        _;
+    }
+
+    /// @dev Proposing the addition of a new token
+    /// @param _token token address that is proposed
+    /// @param _annualRate annual interest rate of the proposed token
+    function proposeNewToken(
+        address _token,
+        uint16 _annualRate
+    ) external onlyAgent {
+        if (tokenAnnualRates[_token] != 0) revert InvalidToken();
+        if (
+            IERC20(_token).balanceOf(address(this)) <
+            requiredTokenBalanceUponAdding
+        ) revert InsufficientBalance();
+
+        uint votesNeededToPass = _calculateVotesNeededToPass();
+        newTokenProposals[newTokenProposalsCount] = NewTokenProposal(
+            newTokenProposalsCount,
+            _token,
+            block.timestamp + newTokenProposalDuration,
+            votesNeededToPass,
+            0,
+            1,
+            ProposalStatus.PENDING,
+            _annualRate
+        );
+        emit NewTokenProposed(newTokenProposalsCount);
+        newTokenProposalsCount++;
+    }
+
+    /// @dev Voting for the new token proposal
+    /// @param voter address which votes directly or indirectly (delegate)
+    /// @param id proposal id
+    /// @param isInFavor is in favor or against the proposal
+    function voteNewToken(
+        address voter,
+        uint id,
+        bool isInFavor
+    ) external onlyAgent {
+        if (voter != msg.sender && delegates[voter] == msg.sender)
+            revert InvalidDelegate();
+        if (agentVotes[voter][id] != Vote.NO_VOTE) revert AlreadyVoted();
+        NewTokenProposal storage proposal = newTokenProposals[id];
+        if (proposal.voteEndsAt == 0) revert ProposalNotFound();
+        if (proposal.voteEndsAt <= block.timestamp) revert VotingEnded();
+        if (isInFavor) {
+            proposal.inFavor++;
+            agentVotes[voter][id] = Vote.IN_FAVOR;
+        } else {
+            proposal.against++;
+            agentVotes[voter][id] = Vote.AGAINST;
+        }
+        emit NewTokenProposalVoted(voter, msg.sender, id, isInFavor);
+        /// @dev If all the agents have voted we automatically calculate count of
+        /// @dev inFavor agents and automatically FAIL the proposal if it's less than
+        /// @dev the count needed for the proposal to pass
+        if (
+            proposal.against + proposal.inFavor >= agentsCount &&
+            proposal.inFavor < proposal.votesNeededToPass
+        ) {
+            proposal.status = ProposalStatus.FAILED;
+            emit NewTokenProposalFailed(id);
+        }
+    }
+
+    /// @dev Executing the new token proposal if the voting time has ended and it passes
+    /// @param id proposal id
+    function executeNewTokenProposal(uint id) external {
+        NewTokenProposal storage proposal = newTokenProposals[id];
+        if (proposal.voteEndsAt == 0) revert ProposalNotFound();
+        if (proposal.voteEndsAt >= block.timestamp) revert VotingInProgress();
+        if (proposal.status != ProposalStatus.PENDING)
+            revert ProposalNotPending();
+        if (
+            IERC20(proposal.token).balanceOf(address(this)) <
+            requiredTokenBalanceUponAdding
+        ) revert InsufficientBalance();
+        /// @dev when the voting period ends, there can be case when not all agents have voted.
+        /// @dev in that case we will decide here if the proposal failed and update the status
+        if (proposal.inFavor < proposal.votesNeededToPass) {
+            proposal.status = ProposalStatus.FAILED;
+            emit NewTokenProposalFailed(id);
+        } else {
+            tokenAnnualRates[proposal.token] = proposal.annualRate;
+            proposal.status = ProposalStatus.EXECUTED;
+            emit NewTokenProposalExecuted(id);
+        }
+    }
+
+    /// @dev Delegate your vote to another agent
+    /// @param _delegateTo address that you are delegating to
+    function deletageVote(address _delegateTo) external onlyAgent {
+        delegates[msg.sender] = _delegateTo;
+    }
+
+    /// @dev Revoking your delegation
+    function revokeDelegate() external onlyAgent {
+        delegates[msg.sender] = address(0);
+    }
+
+    /// @dev Calculates the votes needed for the proposal to pass based on
+    /// @dev current agent count. The number will always be more than half
+    /// @dev of the current agents count
+    function _calculateVotesNeededToPass() private view returns (uint) {
+        return
+            agentsCount % 2 == 0 ? agentsCount / 2 + 1 : (agentsCount + 1) / 2;
+    }
+}
