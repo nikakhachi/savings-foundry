c84b752102267ae73df337af97357baea8f508b6
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-06-25 12:40:51 +0400

    add tests for executing new token proposals

--- test/SavingsAgent.t.sol
+++ test/SavingsAgent.t.sol
@@ -318,6 +318,76 @@ contract SavingsAgentTest is Test {
         savingsAgent.executeNewTokenProposal(id);
     }
 
+    /// @dev test if it reverts if executing non-existing new token proposal
+    function testExecutingInvalidNewTokenProposal() public {
+        vm.expectRevert(SavingsAgent.ProposalNotFound.selector);
+        savingsAgent.executeNewTokenProposal(120);
+    }
+
+    /// @dev test if it reverts if executing new token proposal that's in progress
+    function testExecutingInProgressNewTokenProposal() public {
+        uint proposalId = savingsAgent.newTokenProposalsCount();
+        savingsAgent.proposeNewToken(address(token1), 200);
+        vm.expectRevert(SavingsAgent.VotingInProgress.selector);
+        savingsAgent.executeNewTokenProposal(proposalId);
+    }
+
+    /// @dev test if it reverts if executing new token proposal that's already executed (failed or passed)
+    function testExecutingExecutedNewTokenProposal() public {
+        uint proposalId = savingsAgent.newTokenProposalsCount();
+        savingsAgent.proposeNewToken(address(token1), 200);
+        vm.prank(AGENT_1);
+        savingsAgent.voteNewToken(AGENT_1, proposalId, true);
+        skip(newTokenProposalDuration);
+
+        savingsAgent.executeNewTokenProposal(proposalId);
+
+        vm.expectRevert(SavingsAgent.ProposalNotPending.selector);
+        savingsAgent.executeNewTokenProposal(proposalId);
+    }
+
+    /// @dev test if it reverts if executing new token proposal but if contract has insufficient balance of that token
+    function testExecutingNewTokenProposalWithInsufficientBalance() public {
+        uint proposalId = savingsAgent.newTokenProposalsCount();
+        savingsAgent.proposeNewToken(address(token1), 200);
+        vm.prank(AGENT_1);
+        savingsAgent.voteNewToken(AGENT_1, proposalId, true);
+        skip(newTokenProposalDuration);
+
+        vm.prank(address(savingsAgent));
+        token1.approve(NON_AGENT, TOKEN1_INITIAL_SUPPLY);
+
+        vm.prank(NON_AGENT);
+        token1.transferFrom(
+            address(savingsAgent),
+            NON_AGENT,
+            TOKEN1_INITIAL_SUPPLY
+        );
+
+        vm.expectRevert(SavingsAgent.InsufficientBalance.selector);
+        savingsAgent.executeNewTokenProposal(proposalId);
+    }
+
+    /// @dev test if it reverts if executing new token proposal with token that's already supported
+    function testExecutingNewTokenProposalWithInvalidToken() public {
+        uint proposal1Id = savingsAgent.newTokenProposalsCount();
+        savingsAgent.proposeNewToken(address(token1), 200);
+        vm.prank(AGENT_1);
+        savingsAgent.voteNewToken(AGENT_1, proposal1Id, true);
+        skip(newTokenProposalDuration);
+
+        uint proposal2Id = savingsAgent.newTokenProposalsCount();
+        savingsAgent.proposeNewToken(address(token1), 200);
+        vm.prank(AGENT_1);
+        savingsAgent.voteNewToken(AGENT_1, proposal2Id, true);
+        skip(newTokenProposalDuration);
+
+        savingsAgent.executeNewTokenProposal(proposal1Id);
+
+        vm.expectRevert(SavingsAgent.InvalidToken.selector);
+        savingsAgent.executeNewTokenProposal(proposal2Id);
+    }
+
     /// @dev Helper functions
 
     function _enumToHash(
